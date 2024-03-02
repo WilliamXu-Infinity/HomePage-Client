55de81f67053f75d74a47663c82f07636bbb3478
WilliamXu-Infinity <tingchaoxu@gmail.com>
2024-03-02 13:14:34 -0800

    feat: adding more effect on text

--- src/views/Home/Sections/Intro/index.js
+++ src/views/Home/Sections/Intro/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react"
-import './index.sass'
+import React, { useState, useEffect } from "react"
+import "./index.sass"
+
 // Public files
 import selfImg from "../../../../Asset/img-0.png"
 import resumeLogo from "../../../../Asset/logos/resume.webp"
@@ -7,9 +8,53 @@ import resumeLogo from "../../../../Asset/logos/resume.webp"
 // company logos
 import linkedInLogo from "../../../../Asset/logos/linkedin.png"
 import githubLogo from "../../../../Asset/logos/github.png"
+import hiLogo from "../../../../Asset/hi.png"
+
+function generateStrings(numberOfString, count, string) {
+	const stringLength = string.length - count
+	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // 英文字母
+	const chineseCharacters = "一二三四五六七八九十百千万亿" // 中文字符
+	const japaneseCharacters =
+		"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん" // 日文字符
+	const numbers = "0123456789" // 数字
+	const charMap =
+		characters + chineseCharacters + japaneseCharacters + numbers
+
+	let strings = []
+
+	for (let i = 0; i < numberOfString; i++) {
+		let randomString = ""
+		for (let j = 0; j < stringLength; j++) {
+			const randomChar = charMap.charAt(
+				Math.floor(Math.random() * charMap.length)
+			)
+			randomString += randomChar
+		}
+		strings.push(string.slice(0, count) + randomString)
+	}
+
+	return strings
+}
 
 const Intro = () => {
 	const [isHovered, setIsHovered] = useState(false)
+	const [testString, setTestString] = useState("XXXXXXXXXXXX")
+
+	useEffect(() => {
+		let count = 0,
+			waitTimes = 5
+		const string = "Software Engineer"
+		const interval = setInterval(() => {
+			const strings = generateStrings(1, count, string)
+			setTestString(strings[0])
+			if (waitTimes > 0) {
+				waitTimes--
+			} else count++
+			if (count > string.length) clearInterval(interval)
+		}, 60)
+
+		return () => clearInterval(interval)
+	}, [])
 
 	const handleDownload = () => {
 		const url = window.location.origin + "/TingchaoXu_Resume_2024.pdf"
@@ -32,8 +77,11 @@ const Intro = () => {
 	return (
 		<section className="introContent">
 			<div className="describeSection">
-				<p className="text1 js-scroll slide-top">Software Engineer</p>
-				<p>{"Hi, my name is Tingchao (William) Xu."}</p>
+				<p className="text1 js-scroll slide-top">{testString}
+                <img className="hiLogo" src={hiLogo} alt="hi" />
+                </p>
+                
+                <p>{"Hi, my name is Tingchao (William) Xu."}</p>
 				<p>A passionate Software Engineer based in LA.</p>
 				<div className="companyLogos">
 					<div
